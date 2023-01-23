23b6503729f6a594cdeccb6cb8d51dcfa5348dc2
Patrycjusz bachleda <bachledp@oregonstate.edu>
2023-01-23 09:47:08 -0600

    Adding contacts to datastore

--- datastore-backend/contacts.js
+++ datastore-backend/contacts.js
@@ -5,26 +5,26 @@ const ds = require('./datastore');
 
 const datastore = ds.datastore;
 
-const APPLICATION = "Application";
+const CONTACT = "Contact";
 
 router.use(bodyParser.json());
 
 
 
 /* ------------- Begin Lodging Model Functions ------------- */
-function post_application(name, description, skill) {
-  var key = datastore.key(APPLICATION);
-  const new_application = { "name": name, "description": description, "skill": skill };
-  return datastore.save({ "key": key, "data": new_application }).then(() => { return key });
+function post_contact(name, email, phone, notes) {
+  var key = datastore.key(CONTACT);
+  const new_contact = { "name": name, "email": email, "phone": phone, "notes": notes };
+  return datastore.save({ "key": key, "data": new_contact }).then(() => { return key });
 }
 
 /**
  * The function datastore.query returns an array, where the element at index 0
  * is itself an array. Each element in the array at element 0 is a JSON object
- * with an entity fromt the type "Application".
+ * with an entity fromt the type "Contact".
  */
-function get_applications() {
-  const q = datastore.createQuery(APPLICATION);
+function get_contacts() {
+  const q = datastore.createQuery(CONTACT);
   return datastore.runQuery(q).then((entities) => {
     // Use Array.map to call the function fromDatastore. This function
     // adds id attribute to every element in the array at element 0 of
@@ -35,17 +35,17 @@ function get_applications() {
 
 /**
  * Note that datastore.get returns an array where each element is a JSON object 
- * corresponding to an entity of the Type "Application." If there are no entities
+ * corresponding to an entity of the Type "Contact." If there are no entities
  * in the result, then the 0th element is undefined.
  * @param {number} id Int ID value
  * @returns An array of length 1.
- *      If a application with the provided id exists, then the element in the array
- *           is that application
- *      If no application with the provided id exists, then the value of the 
+ *      If a contact with the provided id exists, then the element in the array
+ *           is that contact
+ *      If no contact with the provided id exists, then the value of the 
  *          element is undefined
  */
-function get_application(id) {
-  const key = datastore.key([APPLICATION, parseInt(id, 10)]);
+function get_contact(id) {
+  const key = datastore.key([CONTACT, parseInt(id, 10)]);
   return datastore.get(key).then((entity) => {
     if (entity[0] === undefined || entity[0] === null) {
       // No entity found. Don't try to add the id attribute
@@ -58,14 +58,14 @@ function get_application(id) {
   });
 }
 
-function put_application(id, name, description, skill) {
-  const key = datastore.key([APPLICATION, parseInt(id, 10)]);
-  const application = { "name": name, "description": description, "skill": skill };
-  return datastore.save({ "key": key, "data": application });
+function put_contact(id, name, email, phone, notes ) {
+  const key = datastore.key([CONTACT, parseInt(id, 10)]);
+  const contact = { "name": name, "email": email, "phone": phone, "notes": notes };
+  return datastore.save({ "key": key, "data": contact });
 }
 
-function delete_application(id) {
-  const key = datastore.key([APPLICATION, parseInt(id, 10)]);
+function delete_contact(id) {
+  const key = datastore.key([CONTACT, parseInt(id, 10)]);
   return datastore.delete(key);
 }
 
@@ -75,39 +75,39 @@ function delete_application(id) {
 
 router.get('/', function (req, res) {
   console.log("Get all request received!");
-  get_applications()
-    .then((applications) => {
-      res.status(200).json(applications);
+  get_contacts()
+    .then((contacts) => {
+      res.status(200).json(contacts);
     });
 });
 
 router.post('/', function (req, res) {
   console.log("Post request received!");
-  post_application(req.body.name, req.body.description, req.body.skill)
+  post_contact(req.body.name, req.body.email, req.body.phone, req.body.notes)
     .then(key => { res.status(201).send('{ "id": ' + key.id + ' }') });
 });
 
 router.put('/:id', function (req, res) {
   console.log("Put request received!");
-  put_application(req.params.id, req.body.name, req.body.description, req.body.skill)
+  put_contact(req.params.id, req.body.name, req.body.email, req.body.phone, req.body.notes)
     .then(res.status(200).end());
 });
 
 router.delete('/:id', function (req, res) {
   console.log("Delete request received!");
-  delete_application(req.params.id).then(res.status(204).end())
+  delete_contact(req.params.id).then(res.status(204).end())
 });
 
 router.get('/:id', function (req, res) {
   console.log("Get request received!");
-  get_application(req.params.id)
-    .then(application => {
-      if (application[0] === undefined || application[0] === null) {
-        // The 0th element is undefined. This means there is no application with this id
-        res.status(404).json({ 'Error': 'No application exists with this id' });
+  get_contact(req.params.id)
+    .then(contact => {
+      if (contact[0] === undefined || contact[0] === null) {
+        // The 0th element is undefined. This means there is no contact with this id
+        res.status(404).json({ 'Error': 'No contact exists with this id' });
       } else {
-        // Return the 0th element which is the application with this id
-        res.status(200).json(application[0]);
+        // Return the 0th element which is the contact with this id
+        res.status(200).json(contact[0]);
       }
     });
 });
--- datastore-backend/index.js
+++ datastore-backend/index.js
@@ -1,4 +1,4 @@
 const router = module.exports = require('express').Router();
 
 router.use('/applications', require('./applications'));
-// router.use('/contacts', require('./contacts'));
+router.use('/contacts', require('./contacts'));
