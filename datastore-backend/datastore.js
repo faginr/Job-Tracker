3d7048399f3d741df06b8c916c07cfbaab380002
Patrycjusz bachleda <bachledp@oregonstate.edu>
2023-01-26 10:21:22 -0600

    added Google Cloud projectId to database.js file

--- datastore-backend/datastore.js
+++ datastore-backend/datastore.js
@@ -1,7 +1,9 @@
 const {Datastore} = require('@google-cloud/datastore');
 
+const projectId ='capstone-project-374922';
+
 module.exports.Datastore = Datastore;
-module.exports.datastore = new Datastore();
+module.exports.datastore = new Datastore({projectId: projectId});
 module.exports.fromDatastore = function fromDatastore(item){
     item.id = item[Datastore.KEY].id;
     return item;
