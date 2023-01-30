fd1940252a3620c7f6cb22042c95eec12abf65b1
philip-peiffer <philip.peiffer@gmail.com>
2023-01-29 17:36:45 -0700

    added routes for skills

--- datastore-backend/index.js
+++ datastore-backend/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const applications = require('./applications');
 const contacts = require('./contacts');
 const users = require('./users');
+const userAppSkills = require('./userAppSkills')
+const userSkills = require('./userSkills')
+const generalSkills = require('./skills')
 
 const router = express.Router()
 
 router.use('/applications', applications);
 router.use('/contacts', contacts);
 router.use('/users', users)
+router.use('/skills', generalSkills)
+router.use('/users/:user_id/', userSkills, userAppSkills)
 
 module.exports = {router}
