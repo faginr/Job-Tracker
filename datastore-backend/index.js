be13e347c791e06bdbf5d0cd92d7386965800af8
Philip Peiffer <philip.peiffer@gmail.com>
2023-01-30 14:00:56 -0700

    moving skills to routes folder

--- datastore-backend/index.js
+++ datastore-backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const applications = require('./routes/applications');
 const contacts = require('./routes/contacts');
 const users = require('./routes/users');
+const generalSkills = require('./routes/skills')
+const userSkills = require('./routes/userSkills')
+const userAppSkills = require('./routes/userAppSkills')
 
 const router = express.Router()
 
