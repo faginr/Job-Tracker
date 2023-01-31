0d35619d81d4bb5f1fb87fd4ce4732d7516680cb
Philip Peiffer <philip.peiffer@gmail.com>
2023-01-31 13:21:43 -0700

    added more error messages

--- datastore-backend/routes/errorMessages.js
+++ datastore-backend/routes/errorMessages.js
@@ -2,10 +2,14 @@
 const errorMessages = {
   400: {jsonError: {"Error": "Problem with JSON format in body."}, 
         keyError: {"Error": "The user sends invalid object keys."},
-        userExists: {"Error": "User already exists"}
+        userExists: {"Error": "User already exists"},
+        valueError: {"Error": "Request body values contain invalid data"}
       },
   403: {"403": "User does not own resource"}, 
-  404: {"Error": "No contact exists with this id."},
+  404: {contacts: {"Error": "No contact exists with this id."},
+        users: {"404": "User does not exist"},
+        skills: {"404": "Skill does not exist"}
+      },
   406: {"Error": "The user sends unsupported Accept header."},
   415: {"Error": "The user sends unsupported Content-Type."}
 }
