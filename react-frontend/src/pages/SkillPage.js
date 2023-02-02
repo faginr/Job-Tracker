d35251fcd1d6574e421d6ab5f72ffd5312440e19
Philip Peiffer <philip.peiffer@gmail.com>
2023-02-02 12:07:59 -0700

    displaying shared skills from ds until user verification enabled

--- react-frontend/src/pages/SkillPage.js
+++ react-frontend/src/pages/SkillPage.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SharedSkills from '../components/SharedSkill';
 
 function AboutPage() {
+  const [skills, setSkills] = useState([])
+
+  async function loadSkills() {
+    const response = await fetch('/skills');
+    const data = await response.json();
+    setSkills(data);
+  }
+
+  useEffect(() => {loadSkills()}, [])
+
   return (
     <>
       <h1>Skill Page</h1>
-
-      <div>
-        TODO
-      </div>
+      <SharedSkills skills={skills} />
     </>
   );
 }
