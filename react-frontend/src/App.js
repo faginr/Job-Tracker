98f62d5aab2c185d61758aecce6db313ff4f9cc6
Philip Peiffer <philip.peiffer@gmail.com>
2023-02-08 09:15:20 -0700

    change feature pane to take up entire height of window

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -21,39 +21,42 @@ function App() {
   const [featureObj, setFeatureObj] = useState()
 
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <h5>Welcome to Job Tracker!</h5>
       </header>
+      
+      <div className="App">
+        <Navigation setFeatureObj={setFeatureObj}/>
 
-      <Navigation setFeatureObj={setFeatureObj}/>
+        <main className="App-main">
+          <div id='main-display'>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              
+              <Route path="/applications" element={<ApplicationPage setApplicationToEdit={setApplicationToEdit} />} />
+              
+              <Route path="/add-application" element={<AddApplicationPage />} />
+              
+              <Route path="/edit-application" element={<EditApplicationPage applicationToEdit={applicationToEdit} />} />
+              
+              <Route path="/skills" element={<SkillPage setFeaturePane={setFeatureObj}/>} />
+              
+              <Route path="/contacts" element={<ContactPage setContactToEdit={setContactToEdit} />} />
 
-      <main className="App-main">
-        <div id='main-display'>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            
-            <Route path="/applications" element={<ApplicationPage setApplicationToEdit={setApplicationToEdit} />} />
-            
-            <Route path="/add-application" element={<AddApplicationPage />} />
-            
-            <Route path="/edit-application" element={<EditApplicationPage applicationToEdit={applicationToEdit} />} />
-            
-            <Route path="/skills" element={<SkillPage setFeaturePane={setFeatureObj}/>} />
-            
-            <Route path="/contacts" element={<ContactPage setContactToEdit={setContactToEdit} />} />
-
-            <Route path="/add-contact" element={<AddContactPage />} />
-            
-            <Route path="/edit-contact" element={<EditContactPage contactToEdit={contactToEdit} />} />
-            
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-        
-        {/* This area controls hiding/unhiding pane on right */}
-        {featureObj}
-      </main>
+              <Route path="/add-contact" element={<AddContactPage />} />
+              
+              <Route path="/edit-contact" element={<EditContactPage contactToEdit={contactToEdit} />} />
+              
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+          
+        </main>
+      </div>
+      
+      {/* This area controls hiding/unhiding pane on right */}
+      {featureObj}
 
       <footer className="App-footer">
         <h4><a href="https://forms.gle/3W7bCuVhibz82q6W6">Request Support</a></h4>
