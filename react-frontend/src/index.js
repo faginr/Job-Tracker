d682173148a1905634d85e13b21698826fdf53e7
Philip Peiffer <philip.peiffer@gmail.com>
2023-02-19 14:14:09 -0700

    add auth0 with navigate to allow redirects

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import {Auth0Provider} from "@auth0/auth0-react";
 import App from './App';
 import './index.css';
+import { Auth0ProviderWithNavigate } from './components/Auth0WithNavigate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider 
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        authorizationParams={{redirect_uri: process.env.REACT_APP_AUTH0_CALLBACK_URL}}>
+      <Auth0ProviderWithNavigate>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </React.StrictMode>
 );
