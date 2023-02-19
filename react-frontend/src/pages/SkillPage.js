3142f055d65a92ec4309c77d5d6168a144b3e1f2
Philip Peiffer <philip.peiffer@gmail.com>
2023-02-19 14:17:35 -0700

    add authentication check to skills page

--- react-frontend/src/pages/SkillPage.js
+++ react-frontend/src/pages/SkillPage.js
@@ -2,12 +2,17 @@ import React, { useEffect, useState } from 'react';
 import UserSkills from '../components/UserSkills';
 import SkillForm from '../components/SkillForm';
 import DisplayButton from '../components/DisplayButton';
-import { user } from '../utils/User';
 import AddSkill from '../components/AddSkill';
-import {datastore_url} from '../utils/Constants';
+import LoadingPage from './LoadingPage';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useAPI } from '../utils/Auth0Functions';
+
+const apiURL = process.env.REACT_APP_API_SERVER_URL
 
 function SkillPage({setFeatureChild}) {
   const [skills, setSkills] = useState({})
+  const {user, isAuthenticated} = useAuth0()
+  const {getTokenFromAuth0} = useAPI()
 
   function splitSkillsByProf(userSkills) {
     const skillsMap = {"high": [], "med": [], "low": []}
@@ -44,31 +49,49 @@ function SkillPage({setFeatureChild}) {
   }
 
   async function loadUserSkills() {
-    const response = await fetch(`${datastore_url}/users/${JSON.parse(user).sub}/skills`, {
-      headers: {
-        'Authorization': `Bearer ${user}`
+
+    // get the access token from Auth0 and redirect back to this page
+    const accessToken = await getTokenFromAuth0({redirectURI: '/skills'})
+
+    // fetch the user's skills once authenticated
+    if (isAuthenticated){
+      let response;
+      try {
+        response = await fetch(`${apiURL}/users/${user?.sub.slice(6)}/skills`, {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        });
+      } catch(err){
+        alert("Uh oh... looks like the server is in-communicado!")
       }
-    });
-    if (response.status !== 200) {
-      // show error page??
-      console.log("Whoops! Fetch to skills failed")
-      return setSkills({})
+      
+      if (response.status !== 200) {
+        alert("Whoops! I failed to get your skills")
+        return setSkills({})
+      }
+      const data = await response.json();
+      setSkills(splitSkillsByProf(data));
     }
-    const data = await response.json();
-    setSkills(splitSkillsByProf(data));
   }
 
-  useEffect(() => {loadUserSkills()}, [])
+  useEffect(() => {
+    loadUserSkills()
+  }, [])
 
-  return (
-    <div id="skills-page">
-      <h1>Your current skills:</h1>
-      <UserSkills userSkills={skills} handleClickAction={setSkillFormAsFeature} />
-      
-      <div>------------------</div>
-      <DisplayButton displayTitle={"Add New Skill"} handleClickAction={setAddSkillAsFeature} />
-    </div>
-  );
+  if (isAuthenticated){
+    return (
+      <div id="skills-page">
+        <h1>Your current skills:</h1>
+        <UserSkills userSkills={skills} handleClickAction={setSkillFormAsFeature} />
+        
+        <div>------------------</div>
+        <DisplayButton displayTitle={"Add New Skill"} handleClickAction={setAddSkillAsFeature} />
+      </div>
+    );
+  } else{
+    return <LoadingPage />
+  }
 }
 
 export default SkillPage;
