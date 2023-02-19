9e8e8a2c19fbbe9c78bd0d3ce89530fca10af48d
Philip Peiffer <philip.peiffer@gmail.com>
2023-02-19 14:16:46 -0700

    split out getting token as useAPI hook that we can share

---
+++ react-frontend/src/utils/Auth0Functions.js
@@ -0,0 +1,30 @@
+import {useAuth0} from '@auth0/auth0-react'
+
+function useAPI() {
+    const {getAccessTokenSilently, loginWithRedirect} = useAuth0()
+
+    const getTokenFromAuth0 = async ({redirectURI}={'redirectURI':'/callback'}) => {
+        console.log('getting the access token');
+        let accessToken = ''
+        try{
+            accessToken = await getAccessTokenSilently()
+        } catch(e) {
+            // TODO: handle errors from getAccessTokenSilently
+            if(e.error === 'login_required'){
+                console.error(e)
+                await loginWithRedirect({
+                    appState: {
+                        returnTo: `${redirectURI}`
+                    }
+                })
+            } else {
+                throw e
+            }
+        }
+        return accessToken
+    }
+
+    return {getTokenFromAuth0}
+}
+
+export {useAPI}
