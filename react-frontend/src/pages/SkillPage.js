53b370a11e1b161072774bf3e38034a2b685ff0e
Philip Peiffer <philip.peiffer@gmail.com>
2023-03-01 11:46:42 -0700

    mod to skillpage to remove state of grouped skills

--- react-frontend/src/pages/SkillPage.js
+++ react-frontend/src/pages/SkillPage.js
@@ -3,17 +3,20 @@ import UserSkills from '../components/UserSkills';
 import AddSkill from '../components/AddSkill';
 import SlidingWindow from '../components/SlidingWindow';
 import ReactButton from '../components/ReactButton';
+import LoadingPage from './LoadingPage';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useAPI } from '../utils/Auth0Functions';
 
 const apiURL = process.env.REACT_APP_API_SERVER_URL
 
 function SkillPage() {
-  const [groupedSkills, setGroupedSkills] = useState({})
   const [skillsModified, setSkillsModified] = useState(0)
   const [skills, setSkills] = useState([])
   const {user, isAuthenticated} = useAuth0()
   const {getTokenFromAuth0} = useAPI()
+
+  // group skills any time page re-renders
+  const groupedSkills = splitSkillsByProf(skills)
 
   function splitSkillsByProf(userSkills) {
     const skillsMap = {"high": [], "med": [], "low": []}
@@ -36,24 +39,28 @@ function SkillPage() {
 
 
   async function loadUserSkills() {
-    const data = await fetchRequests.getUserSkills(user, user)
+    const token = await getTokenFromAuth0('/skills')
+    const data = await fetchRequests.getUserSkills(user, token)
     setSkills(data)
+  
   }
 
   useEffect(() => {loadUserSkills()}, [skillsModified])
-  useEffect(() => setGroupedSkills(splitSkillsByProf(skills)), [skills])
 
   return (
-    <div id="skills-page">
-      <h1>Your current skills:</h1>
-      <UserSkills userSkills={groupedSkills} skillsModified={skillsModified} setSkillsModified={setSkillsModified} />
-      
-      <div>
-        <SlidingWindow 
-          Page={<AddSkill skillAdded={skillsModified} setSkillAdded={setSkillsModified} userSkills={skills}/>}
-          ClickableComponent={<ReactButton label={"Add New Skill"}/>} />
+    isAuthenticated ?
+      <div id="skills-page">
+        <h1>Your current skills:</h1>
+        <UserSkills userSkills={groupedSkills} skillsModified={skillsModified} setSkillsModified={setSkillsModified} />
+        
+        <div>
+          <SlidingWindow 
+            Page={<AddSkill skillAdded={skillsModified} setSkillAdded={setSkillsModified} userSkills={skills}/>}
+            ClickableComponent={<ReactButton label={"Add New Skill"}/>} />
+        </div>
       </div>
-    </div>
+    :
+      <LoadingPage />
   );
 }
 
